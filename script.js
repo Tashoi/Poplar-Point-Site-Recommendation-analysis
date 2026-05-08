b77afc319d93d876f1ff0008e5ec411eeba24049
Tashoi <seonakaye20@gmail.com>
2026-05-08 11:41:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ new Chart(ctx, {
                 data: [47.6, 44.4, 40.1, 30.4],
                 borderColor: '#16a34a',
                 backgroundColor: '#16a34a',
-                tension: 0.3,
                 fill: false,
                 pointRadius: 4
             },
@@ -25,7 +24,6 @@ new Chart(ctx, {
                 data: [24.3, 6.5, 6.7, -2.5],
                 borderColor: '#f59e0b',
                 backgroundColor: '#f59e0b',
-                tension: 0.3,
                 fill: false,
                 pointRadius: 4
             },
@@ -34,7 +32,6 @@ new Chart(ctx, {
                 data: [5.5, -12.9, -12.4, -21.6],
                 borderColor: '#dc2626',
                 backgroundColor: '#dc2626',
-                tension: 0.3,
                 fill: false,
                 pointRadius: 4
             }
